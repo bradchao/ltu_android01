3b2828a109ae37be81c1a53d01da574ca5ac13c4
brad <brad@brad.tw>
2017-11-15 14:37:20 +0800

    201711151437

--- app/src/main/java/com/example/user/brad01/MainActivity.java
+++ app/src/main/java/com/example/user/brad01/MainActivity.java
@@ -9,22 +9,34 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
     private FragmentManager fmgr;
     private FragmentTransaction tran;
+    private Page1Fragment f1;
+    private Page2Fragment f2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        f1 = new Page1Fragment();
+        f2 = new Page2Fragment();
+
         fmgr = getSupportFragmentManager();
+
         tran = fmgr.beginTransaction();
+        tran.add(R.id.container, f1);
+        tran.commit();
 
     }
 
     public void test1(View view){
-
+        tran = fmgr.beginTransaction();
+        tran.replace(R.id.container, f1);
+        tran.commit();
     }
     public void test2(View view){
-
+        tran = fmgr.beginTransaction();
+        tran.replace(R.id.container, f2);
+        tran.commit();
     }
     public void test3(View view){
 
