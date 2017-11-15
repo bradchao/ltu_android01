2524ebef4263e0518631ab4959e5ea50e7d00b80
brad <brad@brad.tw>
2017-11-15 14:20:57 +0800

    201711151420

--- app/src/main/java/com/example/user/brad01/MainActivity.java
+++ app/src/main/java/com/example/user/brad01/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.user.brad01;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fmgr;
+    private FragmentTransaction tran;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fmgr = getSupportFragmentManager();
+        tran = fmgr.beginTransaction();
+
     }
+
+    public void test1(View view){
+
+    }
+    public void test2(View view){
+
+    }
+    public void test3(View view){
+
+    }
+    public void test4(View view){
+
+    }
+
 }
