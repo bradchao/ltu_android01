a4407a4451e3a972c52ca7afdfff31c2285373d5
brad <brad@brad.tw>
2017-11-15 15:10:39 +0800

    201711151510

--- app/src/main/java/com/example/user/brad01/Page1Fragment.java
+++ app/src/main/java/com/example/user/brad01/Page1Fragment.java
@@ -8,19 +8,39 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class Page1Fragment extends Fragment {
     private View mainView;
+    private View b1;
+    private TextView tv1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         if (mainView == null) {
             mainView = inflater.inflate(R.layout.fragment_page1, container, false);
+            tv1 = (TextView)mainView.findViewById(R.id.p1tv1);
+            b1 = mainView.findViewById(R.id.p1b1);
+            b1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showLottery();
+                }
+            });
 
         }
 
         return mainView;
     }
+
+    private void showLottery(){
+        Log.i("brad", "OK1111");
+    }
+
+
+
+
 }
